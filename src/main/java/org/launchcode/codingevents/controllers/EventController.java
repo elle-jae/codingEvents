30e6a24ef6766d2109d189714d4d3a3412f2a950
Ellen Jardon <e@ellens-mbp.lan>
2023-10-19 21:57:01 -0500

    complted 'Create a Model' videos

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,15 +20,12 @@ import java.util.Map;
 @RequestMapping("events")
 public class EventController {
 
-    private static Map<String,String> events = new HashMap<>();
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
-        events.put("Fall Fun", "Fall Festival");
-        events.put("Spooky Season", "Get ready for a Spooktacular time");
-        events.put("Pumpkin Palooza", "Are you ready for EVERYTHING pumpkin, Pumpkin'");
         return "events/index";
     }
 
@@ -39,7 +37,7 @@ public class EventController {
 
     @PostMapping("create")
     public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
-        events.put(eventName, eventDescription);
+        events.add(new Event(eventName,eventDescription));
         return "redirect:/events";
     }
 
